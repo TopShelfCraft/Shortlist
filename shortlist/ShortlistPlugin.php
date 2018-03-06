a403761dec493b124e963fd3032941d39437b72f
Jason Mayo <jason@bymayo.co.uk>
2018-03-06 10:56:59 +0000

    Added Crawler Detect to stop spam submissions (New lists etc)

--- shortlist/ShortlistPlugin.php
+++ shortlist/ShortlistPlugin.php
@@ -4,6 +4,15 @@ namespace Craft;
 
 class ShortlistPlugin extends BasePlugin
 {
+	
+    public function init()
+    {
+	    
+		require_once __DIR__ . '/vendor/autoload.php';
+		parent::init();
+	    
+    }
+    
     function getName()
     {
         return Craft::t('Shortlist');
